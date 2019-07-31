876b637494963e5e45ccd5e3c9af8396dc260f7a
Ayush Shukla <ashukla@groupon.com>
2019-08-01 00:05:36 +0530

    adding iterative soln for max sum in array

---
+++ src/array/MaximumSumContiguousSubsequence.java
@@ -0,0 +1,40 @@
+package array;
+
+//Kadane's algo - https://www.youtube.com/watch?v=86CQq3pKSUw
+public class MaximumSumContiguousSubsequence {
+
+	public static void main(String[] args) {
+		int[] arr = { -1, 3, 4, -5, 6, 7, -3, 2 };
+
+		System.out.println("sum :" + findSum(arr));
+	}
+
+	private static int findSum(int[] arr) {
+		int maxSum = arr[0], sum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			sum = Math.max(sum + arr[i], arr[i]);
+			maxSum = Math.max(maxSum, sum);
+		}
+		return maxSum;
+	}
+
+	// private static int findSum(int[] arr) {
+	// int maxSum = 0;
+	//
+	// for (int i = 0; i < arr.length; i++) {
+	// int currentSum = 0;
+	// for (int j = i; j < arr.length; j++) {
+	// currentSum = currentSum + arr[j];
+	// System.out.println("current sum1 :" + currentSum + " : maxsum : " + maxSum);
+	//
+	// if (currentSum > maxSum) {
+	// maxSum = currentSum;
+	// }
+	// }
+	// System.out.println("current sum2 :" + currentSum);
+	// }
+	// return maxSum;
+	// }
+
+}
